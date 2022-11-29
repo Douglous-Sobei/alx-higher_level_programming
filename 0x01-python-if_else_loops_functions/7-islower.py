f743143b5f55e1db5a7440788c955d6e15432e9d
Douglous Sobei <douglousmangoyi@gmail.com>
2022-11-30 00:06:08 +0300

    7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 7-islower.py
+
+def islower(c):
+    """check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
