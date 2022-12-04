224ad91efe6e0215b25d71acc5ec699e6b783614
Douglous Sobei <douglousmangoyi@gmail.com>
2022-12-05 02:59:30 +0300

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    string_list = list(my_string)
+    for character in string_list:
+        if character == 'c' or character == 'C':
+            string_list.remove(character)
+    return("".join(string_list))
