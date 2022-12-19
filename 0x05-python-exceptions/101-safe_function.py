ade589414b7746e98b282ce08e2230572bbf82fd
Douglous Sobei <douglousmangoyi@gmail.com>
2022-12-19 23:37:02 +0300

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except:
-            result = b + a
-            break
-    return result
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (ZeroDivisionError, IndexError) as e:
+        print("Exception: {}".format(e), file=stderr)
+        return None
