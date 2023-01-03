89267f2545a204e1bdb520c574dd76990499670f
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-03 14:43:07 +0300

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,6 +3,11 @@
 class Rectangle:
     """Represent a rectangle."""
     def __init__(self, width=0, height=0):
+         """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
