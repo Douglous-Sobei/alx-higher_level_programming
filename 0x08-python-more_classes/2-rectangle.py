9e6d61d70fa3a3221cb286139697e6fce5f2c5c2
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-03 14:02:50 +0300

    Area and Perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+class Rectangle:
+    """Represent a Rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+
+        self.width = width
+        self.height = height
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Set the width of the rectangle."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self._width = value
+
+    @property
+    def height(self):
+        """Set the height of the rectangle."""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self._height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
