5f153d5b79a966d0996a3ae96bb9b763eb94366a
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-04 21:13:15 +0300

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,4 @@
+#!/usr/bin/python3
 class LockedClass:
-    def __init__(self):
-        self.__dict__['first_name'] = None
-
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("You are not allowed to set any attribute other than 'first_name'")
+    """You are not allowed to set any attribute other than 'first_name'"""
+    __name__ = ['first_name']
