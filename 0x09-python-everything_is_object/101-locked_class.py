e0a4bb4226dc42f5f20575c49e3cfa04c193e75d
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-04 21:16:57 +0300

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __init__(self):
-        self.__dict__['first_name'] = None
-
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("You are not allowed to set any attribute other than 'first_name'")
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
