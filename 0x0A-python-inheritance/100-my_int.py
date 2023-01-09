812a2aefa9e310bb876ad64383cb97090361095a
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 00:07:29 +0300

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,31 @@
+
+
 #!/usr/bin/python3
-
-""" Defines a function that adds a new attribute
-    to an object if it's possible
-"""
+'''A module containing a rebellious int.
+'''
 
 
-def add_attribute(obj, attr, value):
-    """ adds new attributes """
-    if not hasattr(obj, "__dict__"):
-        raise TypeError("can't add new attribute")
-    setattr(obj, attr, value)
+class MyInt(int):
+    '''Represents a rebellious integer object.
+    '''
+    def __eq__(self, value):
+        '''Checks if the given value is not equal to the
+        value of this object.
+        Args:
+            value (MyInt): The value to be compared against.
+        Returns:
+            bool: True if the value is not equal to the value
+            stored by this object.
+        '''
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        '''Checks if the given value is equal to the value
+        of this object.
+        Args:
+            value (MyInt): The value to be compared against.
+        Returns:
+            bool: True if the value is equal to the value
+            stored by this object.
+        '''
+        return super().__eq__(value)
