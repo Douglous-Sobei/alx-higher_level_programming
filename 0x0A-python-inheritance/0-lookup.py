90c2a58fff2db44d2a05f225263e6e1fcdca821d
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-09 22:18:08 +0300

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-""" module contains: lookup """
+# 0-lookup.py
+"""Defines obj attr lookup func.
+"""
 
 
 def lookup(obj):
-    """
-        returns the list of available attributes
-        and methods of obj
-        Args:
-            obj: object whose attributes and methods
-                 are to be listed
-        Return:
-            list of attributes and methods of obj
+    """Return lst of obj attr.
     """
