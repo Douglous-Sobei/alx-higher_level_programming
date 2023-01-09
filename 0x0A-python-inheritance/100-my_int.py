3693cbe8b94cd4db1baa6f469da78ddf1cbd46fd
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 00:12:35 +0300

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" class MyInt that inherits from int:
-"""
+# 100-my_int.py
+
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """ Class """
-    def __eq__(self, other):
-        return int(str(self)) != other
+    """Invert int operators == and !=."""
 
-    def __ne__(self, other):
-        return int(str(self)) == 
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
