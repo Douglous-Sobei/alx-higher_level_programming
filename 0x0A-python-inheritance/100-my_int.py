74893af211b09d699f6da5daa2ba595f94e8989c
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 00:09:41 +0300

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,31 +1,12 @@
-
-
 #!/usr/bin/python3
-'''A module containing a rebellious int.
-'''
+""" class MyInt that inherits from int:
+"""
 
 
 class MyInt(int):
-    '''Represents a rebellious integer object.
-    '''
-    def __eq__(self, value):
-        '''Checks if the given value is not equal to the
-        value of this object.
-        Args:
-            value (MyInt): The value to be compared against.
-        Returns:
-            bool: True if the value is not equal to the value
-            stored by this object.
-        '''
-        return super().__ne__(value)
+    """ Class """
+    def __eq__(self, other):
+        return int(str(self)) != other
 
-    def __ne__(self, value):
-        '''Checks if the given value is equal to the value
-        of this object.
-        Args:
-            value (MyInt): The value to be compared against.
-        Returns:
-            bool: True if the value is equal to the value
-            stored by this object.
-        '''
-        return super().__eq__(value)
+    def __ne__(self, other):
+        return int(str(self)) == 
