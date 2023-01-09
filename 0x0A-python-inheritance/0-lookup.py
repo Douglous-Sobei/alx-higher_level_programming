5dfe0b555ad380204daa5b05d41fbd3824279037
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-09 22:12:14 +0300

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
+"" defines the looup(obj). """
 def lookup(obj):
-    attributes = []
-    methods = []
-
-    """ Get a list of all attributes and methods """
-    for attr in dir(obj):
-        """ Check if the attribute is a method """
-        if callable(getattr(obj, attr)):
-            methods.append(attr)
-        else:
-            attributes.append(attr)
-
-    """ Return a list of attributes and methods """
-    return attributes + methods
+    """ returns a list of names that are available in the specified object"""
+    return dir(obj)
