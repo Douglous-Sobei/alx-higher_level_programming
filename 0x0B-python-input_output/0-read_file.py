ccdb4cc4c950ad448b064d5dc773239eb07afa6a
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 20:45:02 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-'''A module containing IO functions.
-'''
+"""
+Module for read_file method.
+"""
+
 
 def read_file(filename=""):
-    '''Reads a UTF-8 encoded text file and prints it to stdout.
-    Args:
-        filename (str): The name of the file to read.
-    '''
-    with open(filename, encoding='utf-8') as file:
-        for line in file.readlines():
-            print(line, end='')
+    """
+    Reads text file and prints to STDOUT
+    """
+
+    with open(filename, "r", encoding="UTF-8") as f:
+        for line in f:
+            print(line, end="")
