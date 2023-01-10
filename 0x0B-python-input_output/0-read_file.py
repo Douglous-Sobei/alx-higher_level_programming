8ac4c2186c4a8a0e4e16450a6fd14825707a43d9
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 20:33:50 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
