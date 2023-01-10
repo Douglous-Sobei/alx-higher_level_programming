270d63a64262387a93c97b79a03fc5e7d0b1dba5
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 21:39:28 +0300

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for to_json_to-string method."""
+
+import json
+
+def to_json_string(my_obj):
+    """Return JSON representation of an object."""
+    return json.dumps(my_obj)
