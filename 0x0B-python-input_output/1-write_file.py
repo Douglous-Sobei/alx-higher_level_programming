5087c6025b73021b96c70608fc3c314e957799f0
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 21:25:32 +0300

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,11 @@ Module for write_file method.
 
 
 def write_file(filename="", text=""):
-    """appends a string at the end of a text file (UTF8) and returns the number of characters added:"""
+    """write_file writes a string to a text file.
+    Args:
+        filename (str): name of file.
+        text (str): text to be written.
+    Return: number of bytes written.
+    """
     with open(filename, mode="w", encoding="UTF-8") as f:
         return (f.write(text))
