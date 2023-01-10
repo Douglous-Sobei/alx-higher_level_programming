39169d9e4559074ddf8fb10fbea65e83608c017e
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-10 23:08:29 +0300

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,19 +3,19 @@
 '''
 import sys
 import os
-from importlib import import_module as Use
+from importlib import import_module as using
 
 
 save_to_json_file, load_from_json_file = (
-    Use('5-save_to_json_file').save_to_json_file,
-    Use('6-load_from_json_file').load_from_json_file
+    using('5-save_to_json_file').save_to_json_file,
+    using('6-load_from_json_file').load_from_json_file
 )
 '''The functions for saving and loading lists.
 '''
-my_list = []
+args_list = []
 '''The list of arguments.
 '''
-file_name = 'add_item.json'
+args_list_file_name = 'add_item.json'
 '''The file name of the file containing the list of arguments.
 '''
 
@@ -23,14 +23,14 @@ file_name = 'add_item.json'
 def run():
     '''Runs the routines of this script.
     '''
-    if not os.path.exists(file_name):
-        with open(file_name, mode='w', encoding='utf-8') as file:
+    if not os.path.exists(args_list_file_name):
+        with open(args_list_file_name, mode='w', encoding='utf-8') as file:
             file.write('[]')
-    json_list = load_from_json_file(file_name)
+    json_list = load_from_json_file(args_list_file_name)
     if (type(json_list) is list) and all(type(s) is str for s in json_list):
-        my_list.extend(json_list)
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, file_name)
+        args_list.extend(json_list)
+    args_list.extend(sys.argv[1:])
+    save_to_json_file(args_list, args_list_file_name)
 
 
 if __name__ == '__main__':
