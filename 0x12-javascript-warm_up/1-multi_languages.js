5dbd954da9749e3c7e95009f515be40ffc4d9002
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-13 17:27:00 +0300

    upadated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const my_array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-console.log(my_array[0]);
-console.log(my_array[1]);
-console.log(my_array[2]);
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
