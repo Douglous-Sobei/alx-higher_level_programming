5dac08d0f33ecadfe26333bbafc72683a39d85a4
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-14 02:47:10 +0300

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log("0");
+  console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
   console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
