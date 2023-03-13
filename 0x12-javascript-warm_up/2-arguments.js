12ae3e3ad6bd9ce368e384b840dce1b2bbd05ec2
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-13 17:51:04 +0300

    updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const len = process.argv.length;
 
 if (len < 3) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (len === 3) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
