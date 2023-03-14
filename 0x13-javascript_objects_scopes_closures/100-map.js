5aed8480e75a1a91771a06bc6295c3e4171ca91a
Douglous Sobei <douglousmangoyi@gmail.com>
2023-03-15 01:54:34 +0300

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const list = require('./100-data').list;
+const newList = list.map((item, index) => item * index);
+
+console.log(list);
+console.log(newList);
