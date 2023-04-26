022207fb6ecfd45cbb6680fd69018e688b436a08
Douglous Sobei <douglousmangoyi@gmail.com>
2023-04-26 06:09:54 +0300

    new update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,10 +7,10 @@ const url = process.argv[2];
 const filename = process.argv[3];
 
 request(url, (err, response, body) => {
-    if (err) console.log(err);
-    else {
-        fs.writeFile(filename, body, 'utf8', (err) => {
-            if (err) console.log(err);
-        });
+  if (err) console.log(err);
+  else {
+    fs.writeFile(filename, body, 'utf8', (err) => {
+      if (err) console.log(err);
+    });
   }
 });
